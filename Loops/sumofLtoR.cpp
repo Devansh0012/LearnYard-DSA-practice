6e60e438a60134ab2ed4edd1b3086a4bcdd86ad9
Devansh0012 <devanshdubey0012@gmail.com>
2024-01-08 22:21:30 +0530

    added comments for sumofLtoR

--- Loops/sumofLtoR.cpp
+++ Loops/sumofLtoR.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    int l,r;
-    cin>>l>>r;
-    for(int num = l; num <= r; num++){
-        int sum = 0;
-        for(int rem = num; rem > 0; rem /= 10){
+    int l,r; //Declared the left and right most numbers
+    cin>>l>>r; // took the range from the user
+    for(int num = l; num <= r; num++){ //loop from l to r
+        int sum = 0; // initialize sum = 0
+        for(int rem = num; rem > 0; rem /= 10){ //sum of digits loop
             sum = sum + rem % 10;
         }
         cout<<"Sum of digits of the number "<<num<<"is "<<sum<<endl;
