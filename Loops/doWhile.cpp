6cbbf7d0f58e02408e93c0de1d4f2cace81a9239
Devansh0012 <devanshdubey0012@gmail.com>
2024-01-08 23:33:23 +0530

    do while loop

---
+++ Loops/doWhile.cpp
@@ -0,0 +1,15 @@
+//do while loop example
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    do{
+        cout<<"This is iteration number "<<i<<endl;
+        i++;
+    }while(i<=n);
+    return 0;
+}
