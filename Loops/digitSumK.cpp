af4b4ab86c8d5782d533eb93a8349b39299b054b
Devansh0012 <devanshdubey0012@gmail.com>
2024-01-10 21:52:26 +0530

    added comments in the code

--- Loops/digitSumK.cpp
+++ Loops/digitSumK.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * This program prints the first 'n' numbers whose sum of digits is equal to 'k'.
+ * It takes input for 'n' and 'k' from the user and uses a while loop and nested for loop to find and print the numbers.
+ */
 int main(){
     int count = 0;
     int number = 1;
     int k;
     int n;
-    cout<<"Enter value of n: ";
+    cout<<"Enter value of n: "; //numbers to be printed
     cin>>n;
-    cout<<"Enter value of k: ";
+    cout<<"Enter value of k: "; //sum of digits which should be k
     cin>>k;
     while(count<n){
         int sum = 0;
