f8a1bc27bd7853dbc77d8f44132c5db3990edf7e
Devansh0012 <devanshdubey0012@gmail.com>
2024-01-24 21:44:54 +0530

    added Functions

---
+++ Functions/Declaring.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Declaring a function
+// return_type function_name (parameter1, parameter2, ...){
+//   // code
+// }
+void printHelloWorld() {
+    cout << "Hello World" << endl;
+    }
+    int main(){
+    printHelloWorld();
+    return 0;
+    }
+    /*
+    Output:
+    Hello World
+    */
