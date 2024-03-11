fd1a54cac1d5bc120d9c2df3fb3c05f6e434bce3
Devansh0012 <devanshdubey0012@gmail.com>
2024-03-11 19:52:18 +0530

    added a new approach to solve this question

--- Loops/PatternBased/SerialNo.cpp
+++ Loops/PatternBased/SerialNo.cpp
@@ -15,3 +15,34 @@ int main(){
     }
   return 0;
 }
+
+
+/*
+Another approach:
+We can use a single loop to print this  pattern.
+for(int i = 1; i <= rows*columns; i++){
+    cout<<i<<" ";
+    if(i%columns == 0){
+        cout<<endl;
+    }
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int rows, columns;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> columns;
+    for(int i = 1; i<=rows;i++){
+        for(int j = 1; j<=columns;j++){
+            cout<<i+j-1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+*/
